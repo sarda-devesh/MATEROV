51f429db4843bd7602354e0bca9975f8564505c8
sarda-devesh <32046390+sarda-devesh@users.noreply.github.com>
2019-03-06 21:04:01 -0800

    Update preTest.py

--- preTest.py
+++ preTest.py
@@ -1,3 +1,5 @@
+#This is the program to detect the size of the objects
+
 from scipy.spatial import distance as dist
 from imutils import perspective
 from imutils import contours
@@ -5,8 +7,11 @@ import numpy as np
 import argparse
 import imutils
 import cv2
+#A function to find the midpoint of two given points 
 def midpoint(ptA, ptB):
 	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+
+#Define the variables
 image = cv2.imread("testuno.jpeg", flags=cv2.IMREAD_COLOR)
 scale_percent = 15
 width = int(image.shape[1] * scale_percent / 100) 
@@ -21,40 +26,38 @@ edged = cv2.erode(edged, None, iterations=1)
 cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-# sort the contours from left-to-right and initialize the
-# 'pixels per metric' calibration variable
+# Sort the contours from left-to-right and initialize the
 (cnts, _) = contours.sort_contours(cnts)
 pixelsPerMetric = 1.0
 orig = image.copy()
 # loop over the contours individually
 for c in cnts:
-	# if the contour is not sufficiently large, ignore it
+	# If the contour is not sufficiently large, ignore it
 	if cv2.contourArea(c) < 1000:
 		continue
-	# compute the rotated bounding box of the contour
+	# Compute the rotated bounding box of the contour
 	box = cv2.minAreaRect(c)
 	box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
 	box = np.array(box, dtype="int")
-
-	# order the points in the contour such that they appear
+	# Order the points in the contour such that they appear
 	# in top-left, top-right, bottom-right, and bottom-left
 	# order, then draw the outline of the rotated bounding
 	# box
 	box = perspective.order_points(box)
 	cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
 
-	# loop over the original points and draw them
+	# Loop over the original points and draw them
 	for (x, y) in box:
 		cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
 
-	# unpack the ordered bounding box, then compute the midpoint
+	# Unpack the ordered bounding box, then compute the midpoint
 	# between the top-left and top-right coordinates, followed by
 	# the midpoint between bottom-left and bottom-right coordinates
 	(tl, tr, br, bl) = box
 	(tltrX, tltrY) = midpoint(tl, tr)
 	(blbrX, blbrY) = midpoint(bl, br)
 
-	# compute the midpoint between the top-left and top-right points,
+	# Compute the midpoint between the top-left and top-right points,
 	# followed by the midpoint between the top-righ and bottom-right
 	(tlblX, tlblY) = midpoint(tl, bl)
 	(trbrX, trbrY) = midpoint(tr, br)
@@ -65,29 +68,26 @@ for c in cnts:
 	cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
 	cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
 
-	# draw lines between the midpoints
+	# Draw lines between the midpoints
 	cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
 		(255, 0, 255), 2)
 	cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
 		(255, 0, 255), 2)
 
-	# compute the Euclidean distance between the midpoints
+	# Compute the Euclidean distance between the midpoints
 	dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
 	dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
 
-	# if the pixels per metric has not been initialized, then
-	# compute it as the ratio of pixels to supplied metric
-	# (in this case, inches)
-
-	# compute the size of the object
+	# Compute the size of the object
 	dimA = dA / pixelsPerMetric
 	dimB = dB / pixelsPerMetric
-
+	#Display the size of the object next to it
 	cv2.putText(orig, "{:.1f}px".format(dimA),
 		(int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
 		0.65, (255, 255, 255), 2)
 	cv2.putText(orig, "{:.1f}px".format(dimB),
 		(int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
 		0.65, (255, 255, 255), 2)
+#Display the size and midpoints for all objects in the image
 cv2.imshow("Image", orig)
 cv2.waitKey(0)
